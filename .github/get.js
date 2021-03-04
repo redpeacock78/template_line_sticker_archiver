2b8c7d53735b894f5df0aa628f48df1e6829933d
redpeacock78 <scarletpeacock78@gmail.com>
2021-03-04 22:52:10 +0900

    Change content: .github/get.js

--- .github/get.js
+++ .github/get.js
@@ -16,7 +16,12 @@ const URL = process.argv[2];
     await page.setExtraHTTPHeaders({ 'Accept-Language': 'ja-JP' });
     await page.goto(URL);
     const result = await page.$$eval("section > div > ul > li > a", list => list.map(elm => elm.href));
-    result.length > 0 ? console.log(result.join('\n')) : "";
+    const index = ['javascript:;'];
+    result.length > 0 ? console.log(
+        result.filter(v => {
+            return !index.includes(v)
+        }).join('\n')
+    ) : "";
     await browser.close();
 }
 )();
