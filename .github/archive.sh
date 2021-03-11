7f5515dfabf45ba3f74ceb228487ae2b7eccf224
redpeacock78 <scarletpeacock78@gmail.com>
2021-03-11 21:41:40 +0900

    Change content: .github/archive.sh

--- .github/archive.sh
+++ .github/archive.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-find stickers -type d -empty -print0 |
-    xargs -0 rm -rf {} &&
-    rar a -rr5% -r -v100m "${*}.rar" "stickers"
+rar a -rr5% -r -v100m "${*}.rar" "stickers"
